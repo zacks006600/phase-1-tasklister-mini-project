95225a2996a40533db3ff0f681c6f8fe2009e4d4
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-10-15 14:16:59 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    buildToDo(e.target['new-task-description'].value)
+    form.reset()
+  });
 });
+  
+// create a fn that builds in the previous block
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.querySelector('#list').appendChild(p)
+  //OR?   document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
